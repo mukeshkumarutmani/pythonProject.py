1aeb03ae27ffad49a5d86f5ad64e6619f4068fad
Mukesh Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-07-27 00:32:49 +0500

    Update pythonProject.py

--- pythonProject.py
+++ pythonProject.py
@@ -20,12 +20,63 @@ print("Hello"+name)
 hero=input(" what is your super hero name")
 print(' My superhero name is '+hero)
 
-typformat
+# typform
 lastYear_Marks=input("My last year makrs is")
 # typeform
 # int(lastYear_Marks)
 new_Marks=int(lastYear_Marks)+20
 print(new_Marks)
+
+# sum program
+first = input("Enter first number")
+second=input("enter second number")
+sum=int(first)+ int(second)
+print(sum)
+
+String Useage
+name = "Elon musk "
+print(name.upper())
+print(name)
+print(name.lower())
+print(name.find('S'))   #it return -1
+print(name.find('usk'))
+print(name.find('Elon'))
+
+print(name.replace('musk','kumar'))
+print(name)
+print(name.replace('E', 'M'))
+print('E' in name)
+print('x' in name)
+print('Elon' in name)
+
+# Operators
+print(6+8)
+print(6*7)
+print(5/10)
+print(20//7)  #it will print only integer
+print(6%4)
+print(5**2)  #power 
+i=5
+i=i+7
+i+=7
+i-=7
+i*=3
+i//=18
+print(i)
+
+#operator percendence
+total=7+7*4
+print(total)
+
+# camparison operator
+print(3<2)
+print(3>=2)
+print(3==3)
+print(2!=4)
+print(2==6 and 3==3)
+print(2==6 or 3==3)
+print(not 9>8)
+
 
 
 
