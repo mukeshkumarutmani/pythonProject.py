3bffbf22f0d61f3b0f4539c23ef399129737791b
[Mukesh Kumar Utmani] <[kumarmukesh789123@gmail.com]>
2023-07-26 23:12:32 +0500

    first commit

---
+++ pythonProject.py
@@ -0,0 +1,32 @@
+print ("Kumar")
+age=78.78
+bol=True
+name="mukesh"
+name="Google By"
+print(bol)
+
+first_name="Tony"
+last_name=("Stark")
+age=51
+is_Genius=True
+print(first_name)
+print(last_name)
+print(age)
+print(is_Genius)
+
+name=input('what is your name ')
+print("Hello"+name)  
+
+hero=input(" what is your super hero name")
+print(' My superhero name is '+hero)
+
+typformat
+lastYear_Marks=input("My last year makrs is")
+# typeform
+# int(lastYear_Marks)
+new_Marks=int(lastYear_Marks)+20
+print(new_Marks)
+
+
+
+
